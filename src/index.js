b0b8933c0ca60c18a303aa8c78cdf190f139663b
Suresh Packiry <sureshpackiry73@gmail.com>
2024-03-20 16:34:37 +1100

    figured out asset management

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './home';
 import './styles/main.scss'
-import './assets/scallop-dish.png';
+import scallop from './assets/scallop-dish.png';
 
+const scallopImg = document.getElementById('scallopImg');
+scallopImg.src = scallop
 
 console.log("hello world");
 console.log(12);
