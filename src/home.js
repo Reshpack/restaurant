a90d1bc0661aeebc5599b20dcf29686eb859419e
Suresh Packiry <sureshpackiry73@gmail.com>
2024-03-25 16:34:25 +1100

    making progress on the navbar looking for some ideas

--- src/home.js
+++ src/home.js
@@ -7,3 +7,8 @@ import gitHub from './assets/github.png';
 const gitHubImg = document.getElementById('gitHubImg');
 gitHubImg.src = gitHub
 
+import logo from './assets/navLogo.png';
+
+const navLogo = document.getElementById('navLogo');
+navLogo.src = logo
+
